858da360e40c4ccc24663e432be935ccc29dfce5
vuhieu441 <vuhieu441@gmail.com>
2022-03-02 09:39:02 +0700

    First commit

---
+++ privacy-policy/src/App.js
@@ -0,0 +1,10 @@
+
+import { PrivacyPolicy } from "./privacy-policy/privacyPolicy";
+
+function App() {
+  return (
+    <PrivacyPolicy/>
+  );
+}
+
+export default App;
---
+++ privacy-policy/src/privacy-policy/privacyPolicy.js
@@ -0,0 +1,65 @@
+import './privacyPolicy.css';
+
+export function PrivacyPolicy(){
+    return(
+        <div className="container">
+            
+            <div className="info">
+                <h1>Điều Khoản sử dụng Website</h1>
+                <p> Mục này quy định các Điều khoản sử dụng trang web này. Bằng việc truy cập trang web hoặc bất
+                    kỳ phần nào của trang web, Quý khách đồng ý với các Điều khoản này.</p>
+                <h3>Quyền sở hữu</h3>
+                <p>VUS và các bên khác là chủ sở hữu của các thương hiệu, biểu tượng và các nhãn mác dịch vụ
+                    được hiển thị trên trang web này. Không được phép sử dụng các thương hiệu biểu tượng và nhãn
+                    mác dịch vụ đó nếu không có sự đồng ý bằng văn bản của VUS hoặc bên sở hữu chúng.
+                    Các tài liệu trên trang web này được bảo vệ bản quyền. Không ai được sửa đổi, sao chép, ghi lại
+                    trong bộ nhớ điện tử, chuyển dịch, mô phỏng, phát tán, sử dụng vì các mục đích thương mại hay
+                    công cộng bất kỳ phần nào của những tài liệu này mà không được VUS chấp thuận trước bằng văn
+                    bản.
+                    </p>
+                <h3>Không bảo đảm</h3>
+                <p>Tài liệu, bao gồm văn bản, hình ảnh, liên kết hoặc các mục khác trên trang web này được cung cấp
+                    theo đúng nguyên trạng, theo nguyên trạng được cung cấp. VUS không bảo đảm tính chính xác,
+                    phù hợp và hoàn thiện của tài tiệu, và trong giới hạn được pháp luật cho phép, từ bỏ trách nhiệm
+                    đối với các lỗi hoặc thiếu sót trong tài liệu. Trừ những bảo đảm không thể bị loại trừ theo quy định
+                    của pháp luật, không có bất kỳ bảo đảm theo bất kỳ loại nào được đưa ra đối với trang web này và
+                    tài liệu, dù là ngầm hiểu, thể hiện rõ, hoặc theo quy định, bao gồm nhưng không giới hạn bởi, các
+                    bảo đảm không vi phạm quyền, quyền sở hữu của bên thứ ba, hoặc bảo đảm máy tính không bị
+                    nhiễm virút.</p>
+                    <h3>Rủi ro và cách thức xử lý</h3>
+                    <p>Các bên được miễn trừ trách nhiệm khi xảy ra tình trạng gián đoạn hoặc chậm trễ thực hiện bất kỳ
+                        nghĩa vụ được nêu trong các Điều khoản và điều kiện này do các nguyên nhân bao gồm nhưng
+                        không giới hạn bởi lệnh khẩn của Chính phủ, hoạt động của tòa án hoặc chính phủ, các quy định
+                        khẩn cấp, các hành động phá hoại, bạo loạn, khủng bố, đình công, tranh chấp lao động, thiên tai,
+                        cháy nổ, mất điện, sự cố nghiêm trọng liên quan đến phần cứng hoặc phần mềm máy tính, giao trễ
+                        các thiết bị, hành động của bên thứ ba, hoặc sự chậm trễ, gián đoạn trong quá trình thực hiện vượt
+                        quá khả năng kiểm soát của đương sự.Trường hợp phát sinh vấn đề nghiêm trọng khiến cho hệ
+                        thống máy tính của phụ huynh và học viên hư hỏng một phần hoặc toàn bộ mà không phải do lỗi
+                        của VUS hoặc phụ huynh / học viên, VUS sẽ hỗ trợ phụ huynh và học viên trong việc phục hồi lại
+                        các dữ liệu đã mất hoặc hư hại.</p>
+                    <h3>Thư điện tử</h3>
+                    <p>VUS không đảm bảo rằng các thư điện tử gửi đến VUS qua Internet sẽ được bảo đảm một cách
+                        hoàn toàn. VUS sẽ không chịu trách nhiệm đối với bất kỳ thiệt hại nào mà người sử dụng phải chịu
+                        nếu người sử dụng gửi thư điện tử cho VUS, hoặc nếu VUS gửi thư điện tử qua Internet cho người
+                        sử dụng theo yêu cầu của họ. VUS không chịu trách nhiệm dưới bất kỳ hình thức nào đối với các
+                        thiệt hại trực tiếp, gián tiếp, đặc biệt hoặc hậu quả phát sinh từ việc sử dụng trang web này.
+                        </p>
+                    <h3>Điều chỉnh</h3>
+                    <p>Tùy từng thời điểm có quyền điều chỉnh các Điều khoản và điều kiện này bằng cách thông báo trên
+                        website</p>
+                    <h3>Cam kết thực hiện và giải quyết tranh chấp</h3>
+                    <p>Hai bên cam kết nghiêm chỉnh thực hiện các Điều khoản và điều kiện. Bất kỳ bất đồng nào phát sinh
+                        trong quá trình thực hiện sẽ được giải quyết theo nguyên tắc thương lượng và hòa giải.Nếu bất
+                        đồng không thể được giải quyết theo phương pháp thương lượng hòa giải thì sẽ được Tòa án có
+                        thẩm quyền giải quyết.</p>
+                    <h3>Luật áp dụng</h3>
+                    <p>Những Điều khoản và Điều kiện này sẽ được thi hành dựa theo Luật hiện hành của Việt Nam.Tất cả
+                        Điều khoản và Điều kiện được làm thành bản Tiếng Việt và Tiếng Anh.Trong trường hợp có bất kỳ
+                        xung đột nào giữa hai bản, bản tiếng Việt sẽ được sử dụng.</p>
+                    <label className="closeBtn" for="close">
+                        <span>Đóng</span>
+                    </label>
+            </div>
+        </div>
+    );
+}
